371f8e561751a8f9f07647990bdafc8da29e5064
Brian Bird <birdb@lanecc.edu>
2022-10-05 11:34:02 -0700

    Partial lab2 stuff

---
+++ BrianBirdSite/BrianBirdSite/Controllers/WelshController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BrianBirdSite.Controllers
+{
+    public class WelshController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Consonants()
+        {
+            return View();
+        }
+    }
+}
